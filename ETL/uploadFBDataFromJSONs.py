5502709b264b4c296d134a0bc9e312190ae2f6e3
nicolaspl <brzezinski.m@vp.pl>
2017-07-06 10:16:39 +0200

    Create uploadFBDataFromJSONs.py

---
+++ ETL/uploadFBDataFromJSONs.py
@@ -0,0 +1,20 @@
+def uploadFBUserFromJSONs(zagregowana tabela):
+    # nic nie zwraca, tylko ładuje dane do bazy
+    
+def uploadFBLikesFromJSONs(zagregowana tabela): 
+  # nic nie zwraca, tylko ładuje dane do bazy
+  
+def uploadFBPhotosFromJSONs(zagregowana tabela):
+  # nic nie zwraca, tylko ładuje dane do bazy
+
+def uploadFBPostsFromJSONs(zagregowana tabela):
+  # nic nie zwraca, tylko ładuje dane do bazy
+  
+
+def uploadFBDataFromJSONs():
+  uploadFBUserFromJSONs(zagregowana tabela)
+  uploadFBLikesFromJSONs(zagregowana tabela)
+  uploadFBPhotosFromJSONs(zagregowana tabela)
+  uploadFBPostsFromJSONs(zagregowana tabela)
+  
+  
