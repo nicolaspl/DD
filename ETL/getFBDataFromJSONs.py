6b7420c1609067cbc1f19fe3f70657b0e5f2a016
nicolaspl <brzezinski.m@vp.pl>
2017-07-06 10:21:50 +0200

    Update getFBDataFromJSONs.py

--- ETL/getFBDataFromJSONs.py
+++ ETL/getFBDataFromJSONs.py
@@ -1,17 +1,28 @@
-def getFBDataFromJSONs():
 
+
+def getFBUserDataFromJSONs():  
   ## przeszukaj S3 pod kątem JSONów z danymi usera i utwórz listę z adresmi do plików
   FBUserJSONsList=...
   ## z każdego pliku wczytaj JSONa i pobierz z niego dane funkcją getFBUserFromJSON() (pętla po liście, appendowanie danych wynikowych)
   ## połącz rezultat przetwarzania wszystkich JSONów w jedną tabelę (lub więcej, jeśli funkcja zwraca tabelki pomocniczne
+  ## zwróć tabele wynikowe
+
+def getFBPicsDataFromJSONs():  
   
-  #wykonaj analogiczne czynności dla pzostałych rodzajów JSONów, wykorzystując:
-  FBPicsData=getFBPicsFromJSON()
-  FBLikesData=getFBLikesFromJSON() 
-  getFBPostsFromJSON()
+def getFBLikesDataFromJSONs():  
+  
+def getFBPostsDataFromJSONs():  
+  
+  
+def getFBDataFromJSONs():
+
+  getFBUserDataFromJSONs()
+  getFBPicsFromJSONs()
+  getFBLikesFromJSONs() 
+  getFBPostsFromJSONs()
   
   #ponieważ poszczególne 4 funkcje mogą zasilać te same tabelki docelowe, użyj funkcji matchFBDataTables(), żeby połączyć te df (np. likes z User z Likes)
   #zwróć zestaw dataframeów, zawierający wszystkie dane ze wszystkich plików JSON
-  #ta funkcja powinna zawierać max 15 linijek kodu- przeszukiwanie S3 pod kątem JSONów danego typu i ich parsowanie należy zapakować w oddzielne funkcję i tutaj tlko je wywołać
+  #ta funkcja powinna zawierać max 15 linijek kodu
   
   
