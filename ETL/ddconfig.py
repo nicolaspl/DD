f171de1e8ce157df6fc4a618b71eeb7816c72ee5
nicolaspl <brzezinski.m@vp.pl>
2017-07-06 10:43:05 +0200

    Add files via upload

---
+++ ETL/ddconfig.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 11:21:20 2017
+
+@author: Mikołaj
+"""
+
+############################################################
+################## RDS CONFIG ##############################
+rds_host = 'deepdoc.ctjllxyunqbm.eu-west-1.rds.amazonaws.com'
+db_username = "bmlynarz"
+db_password = "deepmlynarzlove"
+db_name = "deepdoc2" 
+
+############################################################
+################### S3 CONFIG ##############################
+
+source_data_bucket='deepdocpics'
+ids4reklabelsproc='ids4reklabelsproc'
+ids4rekfacesproc='ids4rekfacesproc'
+ids4hsvproc='ids4hsvproc'
+
+tempcsvpath='/tmp/file.csv'
+tempjpgpath='/tmp/file.jpg'
+
+#tempcsvpath='D:/DeepDoc/Sandbox/file.csv'
+#tempjpgpath='D:/DeepDoc/Sandbox/file.jpg'
