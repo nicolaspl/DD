e6f88cd04665f761f47cd56023ad6aca757215c0
nicolaspl <brzezinski.m@vp.pl>
2017-07-06 10:17:13 +0200

    Update matchFBDataTables.py

--- ETL/matchFBDataTables.py
+++ ETL/matchFBDataTables.py
@@ -1,3 +1,3 @@
 def matchFBDataTables(wyniki wyniki przetwarzania wszystkich JSONów):
-  # weź tabele, będące wynikiem przetwarzania wszystkich plików (zakres tabel dubluje się)
+  # weź tabele, będące wynikiem przetwarzania wszystkich plików (zakres tabel dubluje się pomiędzy wynikami zapytań)
   # zwróć zestaw tabel w takiej postaci, w jakiej będą zasilone do bazy
