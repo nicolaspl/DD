882d8f31ae2475f0d9d2d7acdd9b29d7ed2b71d6
nicolaspl <brzezinski.m@vp.pl>
2017-07-06 09:57:01 +0200

    Create matchFBDataTables.py

---
+++ ETL/matchFBDataTables.py
@@ -0,0 +1,3 @@
+def matchFBDataTables(wyniki wyniki przetwarzania wszystkich JSONów):
+  # weź tabele, będące wynikiem przetwarzania wszystkich plików (zakres tabel dubluje się)
+  # zwróć zestaw tabel w takiej postaci, w jakiej będą zasilone do bazy
