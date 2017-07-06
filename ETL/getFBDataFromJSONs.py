b4143fc0850edd37834d456fb0c12d96b7e8efa8
Nicolaspl42 <statystyka@onet.eu>
2017-07-06 12:05:39 +0200

    szkic getFBData
    szkic modułu pobierającego wszystkie jsony z bucketu na dane facebookowe

--- ETL/getFBDataFromJSONs.py
+++ ETL/getFBDataFromJSONs.py
@@ -1,3 +1,11 @@
+import boto3
+import pandas as pd
+import json
+from botocore.client import Config
+
+################################################################################################
+############################ FUNKCJE POMOCNICZE ################################################
+
 def getFileNamesFromBucket(bucket,extension):
     ''' zwraca df z nazwami plikow o danym rozszerzeniu, ktore wystepuja w konkretnym buckecie i jego podfolderach'''
     
@@ -15,32 +23,23 @@ def getFileNamesFromBucket(bucket,extension):
     
     return result
 
-def scrapBucket4JSONs(bucket,extension):
+def getJSONFromBucket(bucket,filename):
+    ''' wczytuje danego jsona i zwraca go'''
+    try:
+        s3=boto3.client('s3',config=Config(signature_version='s3v4'))
+        json_object=s3.get_object(Bucket=bucket,Key=filename)['Body']
+        plik=json.load(json_object)
+        return plik
 
-    JSONs=getFileNamesFromBucket(bucket,extension)['File'].tolist()
-    JSONs_data=[]
-
-    for i in range(len(JSONs)):
-        photo_list=getPhotoLisFromJSON(bucket,JSONs[i])
-        photos_data=getAllPhotoData(photo_list)
-        JSONs_data+=photos_data
-
-    header=['user_id','photo_id','photo_url','photo_filename','photo_created_time','photo_opis','photo_comments_cnt','photo_filter','photo_likes','photo_tags','photo_users_in_foto']
-    df_summary=pd.DataFrame(JSONs_data,columns=header)
-    return df_summary
-
-def ReadCSVFromBucket(filename,bucket):
-    ''' wczytuje csv jako dataframe'''
-    s3=boto3.client('s3',config=Config(signature_version='s3v4'))
-    s3.download_file(bucket, filename, ddconfig.tempcsvpath)
-    result=pd.read_csv(ddconfig.tempcsvpath,encoding='utf-8')
-    return result
-
+################################################################################################
+############################ FUNKCJE WŁAŚCIWE ##################################################
+    
 def getFBUserDataFromJSONs():  
   ## przeszukaj S3 pod kątem JSONów z danymi usera i utwórz listę z adresmi do plików
-  FBUserJSONsList=scrapBucket4JSONs(deepdocfbdata,'user.json')
-  ## z każdego pliku wczytaj JSONa i pobierz z niego dane funkcją getFBUserFromJSON() (pętla po liście, appendowanie danych wynikowych)
-  ## połącz rezultat przetwarzania wszystkich JSONów w jedną tabelę (lub więcej, jeśli funkcja zwraca tabelki pomocniczne
+  FBUserJSONsList=getFileNamesFromBucket('deepdocfbdata','user.json')
+  ## wczytaj każdego JSONa funkcją  getJSONFromBucket (pętla po licie jsonow)
+  ## pobierz z niego dane funkcją getFBUserFromJSON() ( appendowanie danych wynikowych)
+  ## połącz rezultat przetwarzania wszystkich JSONów w jedną tabelę (lub więcej, jeśli funkcja zwraca tabelki pomocniczne)
   ## zwróć tabele wynikowe
 
 def getFBPicsDataFromJSONs():  
@@ -53,7 +52,9 @@ def matchFBDataTables(wyniki wyniki przetwarzania wszystkich JSONów):
   # weź tabele, będące wynikiem przetwarzania wszystkich plików (zakres tabel dubluje się pomiędzy wynikami zapytań)
   # zwróć zestaw tabel w takiej postaci, w jakiej będą zasilone do bazy
   
-# wywołanie procesu
+################################################################################################
+############################ WYWOŁANIE ################################################
+
 def getFBDataFromJSONs():
 
   getFBUserDataFromJSONs()
